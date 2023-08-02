3c7b558a81d796c4bed7b07a1e88c5003cdb47d6
Nicholas Mendez <snickdx@gmail.com>
2023-08-02 19:52:28 +0000

    fixed dependencies & config and test deploy

---
+++ setup.sh
@@ -0,0 +1,10 @@
+npm install -g @angular/cli@13.3
+npm install -g npm-install-peers
+npm install --save @angular/core@13.3
+npm install --save @angular-devkit/build-angular@13.3
+npm install --save rxjs@7.4.0
+npm install --save zone.js@ 0.11.4
+npm install --save --legacy-peer-deps @syncfusion/ej2-schedule@19.4.38 
+npm install --save --legacy-peer-deps typescript@4.4.3
+npm install tslib --save-dev
+npm audit fix
